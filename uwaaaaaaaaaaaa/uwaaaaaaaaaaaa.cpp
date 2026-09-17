5f768f1be3ef9baf778e47392a122d8d721f6730
mutuki kurita <mu22026stuitand@gmail.com>
2026-09-17 14:20:22 +0900

    うわあああああああああああを追加

--- uwaaaaaaaaaaaa/uwaaaaaaaaaaaa.cpp
+++ uwaaaaaaaaaaaa/uwaaaaaaaaaaaa.cpp
@@ -2,10 +2,11 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cout << "うわあああああああああああ" << endl;
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
